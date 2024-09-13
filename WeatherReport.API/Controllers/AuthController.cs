5cdfed09652032c7286e796d9272491b1a9bdd1c
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-09-13 12:21:34 +0400

    App. API. Add Auth Endpoint Draft.

--- WeatherReport.API/Controllers/AuthController.cs
+++ WeatherReport.API/Controllers/AuthController.cs
@@ -1,27 +1,86 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using WeatherReport.Business.DTOs;
 using WeatherReport.Business.Services.Interfaces;
+using WeatherReport.DataAccess.Entities;
 
 namespace WeatherReport.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController (IEmailService emailService) : ControllerBase
+[AllowAnonymous]
+public class AuthController (IEmailService emailService,IServiceUnitOfWork service,IMapper mapper) : ControllerBase
 {
     [HttpPost("send-otp")]
     public async Task<IActionResult> SendOTPEmail(string userEmail)
     {
-        emailService.SendEmailAsync(userEmail, "OTP email", $"{1234}");
+        await emailService.SendEmailAsync(userEmail, "OTP email", $"{1234}");
         return Ok();
     }
     [HttpPost("login")]
     public async Task<IActionResult> LogIn([FromBody] LoginDTO loginDTO)
     {
-        return Ok();
+        var users = await service.SubscriberService.GetAllAsync();
+        var user = users.First(u => u.Username == loginDTO.Username);
+        if(VerifyPassword(loginDTO.Password,user.PasswordHash,user.PasswordSalt))
+        {
+            return Ok(GenerateJwtToken(user.Id,user.Username,user.UserRole));
+        }
+        return BadRequest("Incorrect password or username");
     }
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterDTO registerDTO)
     {
-        return Ok();
+        var subscriberDTO = mapper.Map<SubscriberDTO>(registerDTO);
+        (var passwordHash, var passwordSalt) = HashPassword(registerDTO.Password);
+
+        subscriberDTO.PasswordHash = passwordHash;
+        subscriberDTO.PasswordSalt = passwordSalt;
+
+        var subscriber = await service.SubscriberService.AddAsync(subscriberDTO);
+
+        return Ok(subscriber);
     }
+    private (string passwordHash, string passwordSalt) HashPassword(string password)
+        {
+            using var hmac = new HMACSHA256();
+            var salt = hmac.Key;
+            var passwordHash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            var passwordSalt = Convert.ToBase64String(salt);
+            return (passwordHash, passwordSalt);
+        }
+
+        private bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            using var hmac = new HMACSHA256(Convert.FromBase64String(storedSalt));
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(computedHash) == storedHash;
+        }
+
+        private string GenerateJwtToken(int userId, string username, string role)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ih5GO96Tw3WQJ4pl5jMmwKAwrXfBYRbcRUwp/kqCTJU="));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
 }
--- WeatherReport.Business/obj/Debug/net8.0/WeatherReport.Business.AssemblyInfo.cs
+++ WeatherReport.Business/obj/Debug/net8.0/WeatherReport.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherReport.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06626b106b6fe15ff303f4989b956d79aaaca2fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a0c2bb2d8635ae5e5f2a10e9a7cc3836b9d063")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherReport.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherReport.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
